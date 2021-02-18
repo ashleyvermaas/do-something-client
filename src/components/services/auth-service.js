d95f50fc8ece574d2bf5f20b739ffe6245633134
Ashley Vermaas <ashleyvermaas1@gmail.com>
2021-02-18 15:03:12 +0100

    create auth-service

---
+++ src/components/services/auth-service.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+class AuthService {
+    constructor(){
+        let service = axios.create({
+            baseURL: 'http://localhost:5000/api',
+            withCredentials: true
+        });
+        this.service = service;
+    }
+
+    signup = (username, email, password) => {
+        return this.service.post('/signup', {username, email, password})
+        .then(response => response.data)
+    }
+
+    login = (email, password) => {
+        return this.service.post('/login', {email, password})
+        .then(response => response.data)
+    }
+
+    logout = () => {
+        return this.service.post('/logout', {})
+        .then(response => response.data)
+    }
+
+    loggedin = () => {
+        return this.service.post('/loggedin')
+        .then(response => response.data)
+    }
+}
+
+export default AuthService;
