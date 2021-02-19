1b07978007f62b03f3c895e8d5117d895f7f8057
Ashley Vermaas <ashleyvermaas1@gmail.com>
2021-02-19 14:09:56 +0100

    create ActivitiesList component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Signup from './components/auth/Signup';
 import Login from './components/auth/Login';
 import Navbar from './components/navbars/Navbar';
 import Sidebar from './components/navbars/Sidebar';
+import ActivitiesList from './components/activities/ActivitiesList';
 
 class App extends Component {
   state = {
@@ -26,6 +27,8 @@ class App extends Component {
           <Route exact path="/signup" render={(props) => <Signup {...props} getUser={this.getTheUser} />} />
 
           <Route exact path="/login" render={(props) => <Login {...props} getUser={this.getTheUser} />} />
+
+          <Route exact path="/activities" render={() => <ActivitiesList  getUser={this.getTheUser} />} />
         </Switch>
       </div>
     );
---
+++ src/components/activities/ActivitiesList.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+class ActivitiesList extends Component {
+    state = {
+        listOfActivities: []
+    }
+
+    getAllActivities = () => {
+        axios.get(`http://localhost:5000/api/activities`, { withCredentials: true })
+            .then((responseFromApi) => {
+                this.setState({
+                    listOfActivities: responseFromApi.data
+                })
+            }, error => console.log(error));
+    }
+
+    componentDidMount() {
+        this.getAllActivities()
+    }
+
+    render() {
+        const activities = this.state.listOfActivities.map(activity => {
+            return (
+                <div key={activity._id}>
+                    <Link to={`/activities/${activity._id}`}>
+                        <h4>{activity.title}</h4>
+                    </Link>
+                </div>
+            )
+        })
+        return (
+            <div>
+                {activities}
+            </div>
+        )
+    }
+}
+
+export default ActivitiesList;
