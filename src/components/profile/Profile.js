303e373231629173a2c19194415ff24610e35c28
Ashley Vermaas <ashleyvermaas1@gmail.com>
2021-02-23 11:03:48 +0100

    add functionality to delete profile

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Sidebar from '../navbars/Sidebar';
+import axios from 'axios';
+import AuthService from '../services/auth-service';
 
 class Profile extends Component {
+    constructor(props) {
+        super(props);
+        this.service = new AuthService();
+    }
+
+    logoutUser = () => {
+        this.service.logout()
+            .then(() => {
+                this.props.getUser(null)
+            })
+    }
+
+    deleteProfile = () => {
+        axios.delete(`http://localhost:5000/api/my-profile`, { withCredentials: true })
+        .then(() => {
+            this.logoutUser();
+            this.props.history.push('/');
+        }, (error) => console.log(error))
+    }
+
     render() {
         return (
             <div>
@@ -13,6 +35,7 @@ class Profile extends Component {
                 <p>Email: {this.props.user.email}</p>
 
                 <p><Link to={"/my-profile/edit"}>Edit profile</Link></p>
+                <button onClick={this.deleteProfile}>Delete profile</button>
             </div>
         )
     }
