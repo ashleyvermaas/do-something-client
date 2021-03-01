17afd0c0d1d8d27d4f86c8309a7af858d148bf9c
Ashley Vermaas <ashleyvermaas1@gmail.com>
2021-03-01 13:55:09 +0100

    add checkbox functionality to activities list

--- src/components/activities/ActivitiesList.js
+++ src/components/activities/ActivitiesList.js
@@ -27,26 +27,43 @@ class ActivitiesList extends Component {
         this.state.showForm ? this.setState({ showForm: false }) : this.setState({ showForm: true })
     }
 
+    handleCheckbox = (e) => {
+        const newStatus = e.target.defaultChecked === true ? "To do" : "Completed";
+        axios.put(`${process.env.REACT_APP_API_URL}/activities/${e.target.id}`, {
+            status: newStatus
+        }, { withCredentials: true })
+            .then(
+                this.getAllActivities()
+            ).catch(err => {
+                console.log("An error occurred: ", err);
+            });
+    }
+
     render() {
         const activities = this.state.listOfActivities.map(activity => {
             return (
                 <div key={activity._id}>
+                    <h4>{activity.title}</h4>
+                    <p>{activity.description}</p>
+                    <p>{activity.category}</p>
+                    <input type="checkbox" id={activity._id} defaultChecked={activity.status === "Completed" ? true : false} onChange={(e) => this.handleCheckbox(e)} />
+                    <label for={activity._id}>Completed</label>
                     <Link to={`/activities/${activity._id}`}>
-                        <h4>{activity.title}</h4>
+                        <p>More info</p>
                     </Link>
                 </div>
             )
         })
         return (
             <div>
-                {this.state.showForm ? 
-                <div>
-                    <AddActivity toggleForm={this.toggleForm} getAllActivities={this.getAllActivities}/>
-                </div> 
-                : 
-                <div>
-                    {activities}
-                </div>}
+                {this.state.showForm ?
+                    <div>
+                        <AddActivity toggleForm={this.toggleForm} getAllActivities={this.getAllActivities} />
+                    </div>
+                    :
+                    <div>
+                        {activities}
+                    </div>}
 
                 <button onClick={this.toggleForm}>
                     {this.state.showForm ? "Go back" : "Create Activity"}
