90d1d01543a039001c57af5594b472646792e0bb
Ashley Vermaas <ashleyvermaas1@gmail.com>
2021-03-03 14:45:42 +0100

    create functionality add experience

--- src/components/activities/ActivityDetails.js
+++ src/components/activities/ActivityDetails.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 import Sidebar from '../navbars/Sidebar';
 import EditActivity from './EditActivity';
+import AddExperience from '../experiences/AddExperience';
 
 class ActivityDetails extends Component {
     constructor(props) {
@@ -54,6 +55,9 @@ class ActivityDetails extends Component {
                         <p>{this.state.description}</p>
                         <p>{this.state.category}</p>
                         <p>{this.state.status}</p>
+                        
+                        <AddExperience activity={this.state._id} />
+
                         <button onClick={this.deleteActivity}>Delete</button>
                         <Link to={'/activities'}>
                             <button>Go back</button>
