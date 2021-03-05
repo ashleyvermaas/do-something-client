ee9c82482c5c0e26da19a4b290b572dffa4f255e
Ashley Vermaas <ashleyvermaas1@gmail.com>
2021-03-05 11:28:52 +0100

    debug delete functionality for experiences

--- src/components/experiences/ExperiencesList.js
+++ src/components/experiences/ExperiencesList.js
@@ -4,8 +4,7 @@ import AddExperience from './AddExperience';
 import EditExperience from './EditExperience';
 
 class ExperiencesList extends Component {
-    deleteExperience = (e) => {
-        const experienceId = e.target.experienceId 
+    deleteExperience = (experienceId) => {
         axios.delete(`${process.env.REACT_APP_API_URL}/experiences/${experienceId}`, { withCredentials: true })
             .then(() => {
                 this.props.getActivityDetails();
@@ -18,6 +17,7 @@ class ExperiencesList extends Component {
              <AddExperience activityId={this.props.activityId} getActivityDetails={this.props.getActivityDetails} />
 
                 {this.props.experiences.map(experience => {
+                    const experienceId = experience._id
                     return(
                         <div key={experience._id}>
                             <p>{experience.date}</p>
@@ -25,7 +25,7 @@ class ExperiencesList extends Component {
                             <p>{experience.rating}</p>
                             <img src={experience.imageUrl} />
                             <EditExperience experience={experience} getActivityDetails={this.props.getActivityDetails} /> 
-                            <button onClick={this.deleteExperience} experienceId={experience._id}>Delete experience</button>
+                            <button onClick={() => this.deleteExperience(experienceId)}>Delete experience</button>
                         </div>
                     )
                 })}
